0a0746eba5db7c153460234b1082d3cdf4692f26
Wronqa <kuba.wrona@onet.pl>
2022-10-18 19:13:49 +0200

    Created type script scanner

--- app/screens/SignUp/EmailScreen.tsx
+++ app/screens/SignUp/EmailScreen.tsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Image } from "react-native";
-import RegisterButton from "../../components/UI/RegisterButton";
-import { LinearGradient } from "expo-linear-gradient";
-import { RegisterUserData } from "../../types/types";
-import { useDispatch, useSelector } from "react-redux";
-import { addEmail } from "../../redux/registerSlice";
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native'
+import RegisterButton from '../../components/UI/RegisterButton'
+import { LinearGradient } from 'expo-linear-gradient'
+import { RegisterUserData } from '../../types/types'
+import { useDispatch, useSelector } from 'react-redux'
+import { addItem } from '../../redux/registerSlice'
 
 interface EmailScreenProps {
-  navigation: any;
+  navigation: any
 }
 const EmailScreen = ({ navigation }: EmailScreenProps) => {
-  const dispatch = useDispatch();
-  const count = useSelector((state: RegisterUserData) => state);
+  const dispatch = useDispatch()
+  const count = useSelector((state: RegisterUserData) => state)
 
   const emailHandler = (email: string) => {
-    dispatch(addEmail(email));
-    console.log(count);
-  };
+    dispatch(addItem({ value: 'email', data: email }))
+    console.log(count)
+  }
 
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={["#AD439C", "#FAAEBE"]}
+        colors={['#AD439C', '#FAAEBE']}
         style={styles.linearGradient}
       >
         <View style={styles.whiteContainer}>
@@ -29,84 +29,84 @@ const EmailScreen = ({ navigation }: EmailScreenProps) => {
           <View style={styles.textInputContainer}>
             <Image
               style={styles.icon}
-              source={require("../../images/mailVector.png")}
+              source={require('../../images/mailVector.png')}
             />
             <TextInput
               style={styles.input}
               onChangeText={(newText) => emailHandler(newText)}
-              placeholder="Enter your email"
-              placeholderTextColor="#ABABAB"
+              placeholder='Enter your email'
+              placeholderTextColor='#ABABAB'
             />
           </View>
-          <RegisterButton toScreen="passwordInput" navigation={navigation} />
+          <RegisterButton toScreen='passwordInput' navigation={navigation} />
         </View>
         <Image
           style={styles.bcgHearths}
-          source={require("../../images/Hearts.png")}
+          source={require('../../images/Hearts.png')}
         />
       </LinearGradient>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   linearGradient: {
     borderRadius: 5,
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
   },
   whiteContainer: {
-    backgroundColor: "#FFFFFF",
-    minHeight: "50%",
-    width: "100%",
+    backgroundColor: '#FFFFFF',
+    minHeight: '50%',
+    width: '100%',
     borderBottomRightRadius: 60,
     borderBottomLeftRadius: 60,
-    alignItems: "center",
+    alignItems: 'center',
   },
   title: {
     fontSize: 55,
-    marginTop: "5%",
-    marginBottom: "5%",
-    width: "80%",
+    marginTop: '5%',
+    marginBottom: '5%',
+    width: '80%',
   },
   btnTitle: {
     fontSize: 20,
-    marginTop: "15%",
+    marginTop: '15%',
   },
   textInputContainer: {
-    width: "80%",
-    backgroundColor: "#F7F7F7",
-    height: "15%",
+    width: '80%',
+    backgroundColor: '#F7F7F7',
+    height: '15%',
     padding: 10,
-    marginTop: "15%",
-    marginBottom: "15%",
+    marginTop: '15%',
+    marginBottom: '15%',
     borderRadius: 10,
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
   },
   input: {
-    width: "90%",
-    height: "100%",
-    color: "#ABABAB",
-    borderBottomColor: "#ABABAB",
+    width: '90%',
+    height: '100%',
+    color: '#ABABAB',
+    borderBottomColor: '#ABABAB',
     borderBottomWidth: 1,
     lineHeight: 23,
   },
   icon: {
-    marginTop: "7%",
-    marginRight: "2%",
+    marginTop: '7%',
+    marginRight: '2%',
   },
   btn: {},
   bcgHearths: {
-    position: "absolute",
-    top: "60%",
+    position: 'absolute',
+    top: '60%',
     zIndex: -1,
   },
-});
-export default EmailScreen;
+})
+export default EmailScreen
--- app/types/types.ts
+++ app/types/types.ts
@@ -1,13 +1,14 @@
 export interface RegisterUserData {
-  email: string
-  password: string
-  name: string
-  age: number | null
-  gender: string
-  ///enum
-  searchFor: string ///enum
-  ///photos
-  //hobbies
-  desc: string
-  city: string
+  [index: string]: string | number | null
+  //   email: string
+  //   password: string
+  //   name: string
+  //   age: number | null
+  //   gender: string
+  //   ///enum
+  //   searchFor: string ///enum
+  //   ///photos
+  //   //hobbies
+  //   desc: string
+  //   city: string
 }
