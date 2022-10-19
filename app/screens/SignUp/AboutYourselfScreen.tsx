31cb5c30ffbf486ca7a913af1b17332c51e52172
Wronqa <kuba.wrona@onet.pl>
2022-10-19 19:47:33 +0200

    Registration part 2

--- app/screens/SignUp/AboutYourselfScreen.tsx
+++ app/screens/SignUp/AboutYourselfScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react'
 import {
   StyleSheet,
   Text,
@@ -7,30 +7,38 @@ import {
   Image,
   TouchableWithoutFeedback,
   Keyboard,
-} from "react-native";
-import RegisterButton from "../../components/UI/RegisterButton";
-import { LinearGradient } from "expo-linear-gradient";
-import { RegisterUserData } from "../../types/types";
-import { useDispatch, useSelector } from "react-redux";
-import { addEmail } from "../../redux/registerSlice";
+} from 'react-native'
+import RegisterButton from '../../components/UI/RegisterButton'
+import { LinearGradient } from 'expo-linear-gradient'
+import { RegisterUserData } from '../../types/types'
+import { useDispatch, useSelector } from 'react-redux'
+import { addItem } from '../../redux/registerSlice'
 
 interface EmailScreenProps {
-  navigation: any;
+  navigation: any
 }
 const AboutYourselfScreen = ({ navigation }: EmailScreenProps) => {
-  const dispatch = useDispatch();
-  const count = useSelector((state: RegisterUserData) => state);
+  const [desc, setDesc] = useState('')
 
-  const emailHandler = (email: string) => {
-    dispatch(addEmail(email));
-    console.log(count);
-  };
+  const dispatch = useDispatch()
+  const state = useSelector((state: RegisterUserData) => state)
+
+  useEffect(() => {
+    let isMounted = true
+
+    isMounted && dispatch(addItem({ value: 'desc', data: desc }))
+    console.log(desc)
+
+    return () => {
+      isMounted = false
+    }
+  }, [desc])
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
         <LinearGradient
-          colors={["#AD439C", "#FAAEBE"]}
+          colors={['#AD439C', '#FAAEBE']}
           style={styles.linearGradient}
         >
           <View style={styles.whiteContainer}>
@@ -39,65 +47,70 @@ const AboutYourselfScreen = ({ navigation }: EmailScreenProps) => {
               multiline={true}
               numberOfLines={6}
               style={styles.input}
-              onChangeText={(newText) => emailHandler(newText)}
-              placeholder="About me"
-              placeholderTextColor="#ABABAB"
+              onChangeText={(desc) => setDesc(desc)}
+              value={desc}
+              placeholder='About me'
+              placeholderTextColor='#ABABAB'
             />
-            <RegisterButton toScreen="hobbyInput" navigation={navigation} />
+            <RegisterButton
+              isDisabled={false}
+              toScreen='hobbyInput'
+              navigation={navigation}
+            />
           </View>
           <Image
             style={styles.bcgHearths}
-            source={require("../../images/Hearts.png")}
+            source={require('../../images/Hearts.png')}
           />
         </LinearGradient>
       </View>
     </TouchableWithoutFeedback>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   linearGradient: {
     borderRadius: 5,
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
   },
   whiteContainer: {
-    backgroundColor: "#FFFFFF",
-    minHeight: "64%",
-    width: "100%",
+    backgroundColor: '#FFFFFF',
+    minHeight: '64%',
+    width: '100%',
     borderBottomRightRadius: 60,
     borderBottomLeftRadius: 60,
-    alignItems: "center",
+    alignItems: 'center',
   },
   title: {
     fontSize: 55,
-    marginTop: "5%",
-    marginBottom: "5%",
-    width: "80%",
+    marginTop: '5%',
+    marginBottom: '5%',
+    width: '80%',
   },
   btnTitle: {
     fontSize: 20,
-    marginTop: "15%",
+    marginTop: '15%',
   },
   input: {
-    width: "80%",
-    backgroundColor: "#F7F7F7",
+    width: '80%',
+    backgroundColor: '#F7F7F7',
     height: 120,
-    marginTop: "10%",
-    marginBottom: "15%",
+    marginTop: '10%',
+    marginBottom: '15%',
     fontSize: 16,
   },
   btn: {},
   bcgHearths: {
-    position: "absolute",
-    top: "60%",
+    position: 'absolute',
+    top: '60%',
     zIndex: -1,
   },
-});
-export default AboutYourselfScreen;
+})
+export default AboutYourselfScreen
--- app/screens/SignUp/SearchForScreen.tsx
+++ app/screens/SignUp/SearchForScreen.tsx
@@ -1,35 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import RegisterButton from "../../components/UI/RegisterButton";
-import { LinearGradient } from "expo-linear-gradient";
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import RegisterButton from '../../components/UI/RegisterButton'
+import { LinearGradient } from 'expo-linear-gradient'
+import { useDispatch, useSelector } from 'react-redux'
+import { RegisterUserData } from '../../types/types'
+import { addItem } from '../../redux/registerSlice'
 
 interface EmailScreenProps {
-  navigation: any;
+  navigation: any
 }
 const SearchForScreen = ({ navigation }: EmailScreenProps) => {
-  const [lfGender, setLfGender] = useState("");
+  const [lfGender, setLfGender] = useState('')
 
-  const [isDisabled, setIsDisabled] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  const dispatch = useDispatch()
+  const state = useSelector((state: RegisterUserData) => state)
+
   useEffect(() => {
-    let isMounted = true;
+    let isMounted = true
 
     if (isMounted) {
       if (lfGender.length > 1) {
-        setIsDisabled(false);
+        setIsDisabled(false)
+        dispatch(addItem({ value: 'searchFor', data: lfGender }))
+        console.log(state)
       } else {
-        setIsDisabled(true);
+        setIsDisabled(true)
       }
     }
 
     return () => {
-      isMounted = false;
-    };
-  }, [lfGender]);
+      isMounted = false
+    }
+  }, [lfGender])
 
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={["#AD439C", "#FAAEBE"]}
+        colors={['#AD439C', '#FAAEBE']}
         style={styles.linearGradient}
       >
         <View style={styles.whiteContainer}>
@@ -37,106 +46,106 @@ const SearchForScreen = ({ navigation }: EmailScreenProps) => {
           <TouchableOpacity
             style={styles.btn}
             onPress={() => {
-              setLfGender("Male");
+              setLfGender('Male')
             }}
           >
-            <Text style={lfGender === "Male" ? styles.textM : styles.text}>
+            <Text style={lfGender === 'Male' ? styles.textM : styles.text}>
               Male
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.btn}
             onPress={() => {
-              setLfGender("Female");
+              setLfGender('Female')
             }}
           >
-            <Text style={lfGender === "Female" ? styles.textF : styles.text}>
+            <Text style={lfGender === 'Female' ? styles.textF : styles.text}>
               Female
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.btn}
             onPress={() => {
-              setLfGender("Both");
+              setLfGender('Both')
             }}
           >
-            <Text style={lfGender === "Both" ? styles.textF : styles.text}>
+            <Text style={lfGender === 'Both' ? styles.textF : styles.text}>
               Both
             </Text>
           </TouchableOpacity>
           <RegisterButton
             isDisabled={isDisabled}
-            toScreen="photosInput"
+            toScreen='photosInput'
             navigation={navigation}
           />
         </View>
         <Image
           style={styles.bcgHearths}
-          source={require("../../images/Hearts.png")}
+          source={require('../../images/Hearts.png')}
         />
       </LinearGradient>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   linearGradient: {
     borderRadius: 5,
-    height: "100%",
-    width: "100%",
+    height: '100%',
+    width: '100%',
   },
   whiteContainer: {
-    backgroundColor: "#FFFFFF",
-    minHeight: "70%",
-    width: "100%",
+    backgroundColor: '#FFFFFF',
+    minHeight: '70%',
+    width: '100%',
     borderBottomRightRadius: 60,
     borderBottomLeftRadius: 60,
-    alignItems: "center",
+    alignItems: 'center',
   },
   title: {
     fontSize: 55,
-    marginTop: "5%",
-    marginBottom: "15%",
-    width: "80%",
+    marginTop: '5%',
+    marginBottom: '15%',
+    width: '80%',
   },
   btnTitle: {
     fontSize: 20,
-    marginTop: "15%",
+    marginTop: '15%',
   },
   btn: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "90%",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '90%',
     height: 70,
-    backgroundColor: "transparent",
-    borderColor: "#C04D9F",
+    backgroundColor: 'transparent',
+    borderColor: '#C04D9F',
     borderWidth: 3,
-    marginBottom: "12%",
+    marginBottom: '12%',
     borderRadius: 55,
   },
   text: {
-    color: "#333",
+    color: '#333',
     fontSize: 30,
   },
   textM: {
-    color: "#1c72fc",
+    color: '#1c72fc',
     fontSize: 30,
   },
   textF: {
-    color: "#e317d9",
+    color: '#e317d9',
     fontSize: 30,
   },
   bcgHearths: {
-    position: "absolute",
-    top: "60%",
+    position: 'absolute',
+    top: '60%',
     zIndex: -1,
   },
-});
-export default SearchForScreen;
+})
+export default SearchForScreen
