e7daadcd4c6d6841560767fe25697cbad81d06b5
Bartłomiej Wydrzycki <92253080+Wydreq@users.noreply.github.com>
2022-10-22 23:39:50 +0200

    Fix

--- app/controllers/loginController.ts
+++ app/controllers/loginController.ts
@@ -1,32 +1,32 @@
-import axios from 'axios'
+import axios from "axios";
 
 type Data = {
-  email: string
-  userPassword: string
-}
+  email: string;
+  userPassword: string;
+};
 export const loginCall = async (
   data: Data,
   navigate: (screen: string) => void
 ) => {
   try {
     const res = await axios.post(
-      'http://192.168.1.132:6000/api/auth/login',
+      "http://192.168.8.179:6000/api/auth/login",
       data
-    )
-    console.log('XD')
-    console.log(res.data.success)
-    return true
+    );
+    console.log("XD");
+    console.log(res.data.success);
+    return true;
   } catch (err: any) {
     if (err?.response.data?.accountStatus === false) {
       try {
-        await axios.post('http://192.168.1.132:6000/api/auth/activate/resend', {
+        await axios.post("http://192.168.8.179:6000/api/auth/activate/resend", {
           email: data.email,
-        })
-        navigate('tokenInput')
+        });
+        navigate("tokenInput");
       } catch (err) {
-        return false
+        return false;
       }
     }
-    return false
+    return false;
   }
-}
+};
