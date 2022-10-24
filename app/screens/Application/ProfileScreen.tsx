8c6d7f6f49341405cb4d2340fff2dc8dea841268
Wronqa <kuba.wrona@onet.pl>
2022-10-24 19:26:32 +0200

    Created auto open drawer

--- app/screens/Application/ProfileScreen.tsx
+++ app/screens/Application/ProfileScreen.tsx
@@ -2,15 +2,14 @@ import { Text, TouchableOpacity, View } from 'react-native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import HomeScreen from './HomeScreen'
 import { useEffect } from 'react'
-import { DrawerActions } from '@react-navigation/native'
+import { DrawerActions, useFocusEffect } from '@react-navigation/native'
 
 const ProfileScreen = ({ navigation }: { navigation: any }) => {
   const Drawer = createDrawerNavigator()
 
-  navigation.dispatch(DrawerActions.openDrawer())
-
   return (
     <Drawer.Navigator
+      defaultStatus='open'
       useLegacyImplementation={true}
       initialRouteName='Home'
       screenOptions={{ headerShown: false }}
