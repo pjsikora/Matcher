ea2e4ace55532817ca817103f4e0b6cf78cdcbf8
Wronqa <kuba.wrona@onet.pl>
2022-10-24 17:01:29 +0200

    Saved data

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,6 +1,11 @@
 import registerReducer from './registerSlice'
 import userReducer from './userSlice'
 import { configureStore } from '@reduxjs/toolkit'
+import * as SecureStore from 'expo-secure-store'
+
+async function save(key: string, value: any) {
+  await SecureStore.setItemAsync(key, value)
+}
 
 export const store = configureStore({
   reducer: {
@@ -8,6 +13,10 @@ export const store = configureStore({
     userData: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+})
+
+store.subscribe(() => {
+  save('userData', JSON.stringify(store.getState().userData))
 })
 
 export type RootState = ReturnType<typeof store.getState>
